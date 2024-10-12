d035a849a1989b96a460b321387c720cc1b9868a
Филиппов Данил <filippok2004180@gmail.com>
2024-10-13 01:55:29 +0500

    Add test views

--- tests/views.py
+++ tests/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from tests.models import TestModel
 
@@ -8,5 +8,12 @@ from tests.models import TestModel
 class TestListView(ListView):
     model = TestModel
     template_name = 'tests/all_tests.html'
+    context_object_name = 'tests_list'
+    paginate_by = 10
 
+
+class TestDetailView(DetailView):
+    model = TestModel
+    context_object_name = "test"
+    template_name = "tests/test.html"
 
