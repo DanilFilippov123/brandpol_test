0eba96d77e55953d8c1e5b300309bd536a35163b
Филиппов Данил <filippok2004180@gmail.com>
2024-10-15 19:39:43 +0500

    Change sqlight database to postgres

--- brandpol_test/settings.py
+++ brandpol_test/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'brandpol_test.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "tests",
+        },
     }
 }
 # Password validation
