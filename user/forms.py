cd6790c7559e9b2c13389335ee29864628b24c68
Филиппов Данил <filippok2004180@gmail.com>
2024-10-16 13:53:41 +0500

    Remove email from form

--- user/forms.py
+++ user/forms.py
@@ -1,17 +1,13 @@
-from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'password1', 'password2']
         labels = {
             'username': 'Имя пользователя',
             'password1': 'Пароль',
-            'password2': 'Повторите пароль',
-            'email': 'Емэил',
+            'password2': 'Повторите пароль'
         }
