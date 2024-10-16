aa0779592d21a74eca361e6c36bbc7fa19890f2f
Филиппов Данил <filippok2004180@gmail.com>
2024-10-16 13:54:39 +0500

    Save form

--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView, CreateView
+from django.views.generic import TemplateView, FormView
 
 from tests.models import UserTestHistoryModel
 from user.forms import RegistrationForm
@@ -21,8 +21,13 @@ class UserAccount(LoginRequiredMixin, TemplateView):
         return context
 
 
-class RegistrationFormView(CreateView):
+class RegistrationFormView(FormView):
     template_name = 'registration/registration.html'
     form_class = RegistrationForm
+
+    def form_valid(self, form):
+        res = super().form_valid(form)
+        form.save()
+        return res
 
     success_url = reverse_lazy('user:user_account')
